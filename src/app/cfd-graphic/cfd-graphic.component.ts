a990960448cd2449de878804d1470587de816716
Lucas Marques <lucas.fonseca@itixti.com.br>
2022-06-13 19:04:52 -0300

    inclusão de um exemplo com dados mockados

--- src/app/cfd-graphic/cfd-graphic.component.ts
+++ src/app/cfd-graphic/cfd-graphic.component.ts
@@ -15,8 +15,8 @@ export class CfdGraphicComponent implements OnInit {
 
     const colors = {
       green: {
-        fill: '#e0eadf',
-        stroke: '#5eb84d',
+        fill: '#a5d2a5',
+        stroke: '#329932',
       },
       lightBlue: {
         stroke: '#6fccdd',
@@ -29,56 +29,61 @@ export class CfdGraphicComponent implements OnInit {
         fill: '#8fa8c8',
         stroke: '#75539e',
       },
+      yellow: {
+        fill: '#fff999',
+        stroke: '#fff200'
+      },
+      red: {
+        fill: '#ff7a7a',
+        stroke: '#ff3232'
+      }
     };
 
-    const loggedIn = [26, 36, 42, 38, 40, 30, 12];
-    const available = [34, 44, 33, 24, 25, 28, 25];
-    const availableForExisting = [16, 13, 25, 33, 40, 33, 45];
-    const unavailable = [5, 9, 10, 9, 18, 19, 20];
-    const xData = [13, 14, 15, 16, 17, 18, 19];
+    const labels = ["Sprint 1", "Sprint 2", "Sprint 3", "Sprint 4", "Sprint 5", "Sprint 6", "Sprint 7", "Sprint 8", "Sprint 9", "Sprint 10"];
+    const testeData = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+    const devData = [5, 10, 20, 60, 70, 80, 90, 10, 32, 38];
+    const prontoData = [0, 5, 10, 10, 10, 11, 12, 14, 20, 22];
+    const backlodData = [50, 50, 80, 90, 100, 100, 100, 100, 100, 100];
 
-    var cfd = new Chart("cfd", {
+    const cfd = new Chart("cfd", {
       type: 'line',
       data: {
-        labels: xData,
+        labels: labels,
         datasets: [{
-          label: "Unavailable",
-          fill: true,
-          backgroundColor: colors.purple.fill,
-          pointBackgroundColor: colors.purple.stroke,
-          borderColor: colors.purple.stroke,
-          //pointHighlightStroke: colors.purple.stroke,
-          borderCapStyle: 'butt',
-          data: unavailable,
-
-        }, {
-          label: "Available for Existing",
+          label: "Teste",
           fill: true,
           backgroundColor: colors.darkBlue.fill,
           pointBackgroundColor: colors.darkBlue.stroke,
           borderColor: colors.darkBlue.stroke,
-          //pointHighlightStroke: colors.darkBlue.stroke,
           borderCapStyle: 'butt',
-          data: availableForExisting,
+          data: testeData,
+
         }, {
-          label: "Available",
+          label: "Desenvolvimneto",
           fill: true,
-          backgroundColor: colors.green.fill,
-          pointBackgroundColor: colors.lightBlue.stroke,
-          borderColor: colors.lightBlue.stroke,
-          //pointHighlightStroke: colors.lightBlue.stroke,
+          backgroundColor: colors.yellow.fill,
+          pointBackgroundColor: colors.yellow.stroke,
+          borderColor: colors.yellow.stroke,
           borderCapStyle: 'butt',
-          data: available,
-        }, {
-          label: "Logged In",
+          data: devData,
+        },
+        {
+          label: "Pronto",
           fill: true,
           backgroundColor: colors.green.fill,
           pointBackgroundColor: colors.green.stroke,
           borderColor: colors.green.stroke,
-          //pointHighlightStroke: colors.green.stroke,
-          data: loggedIn,
+          borderCapStyle: 'butt',
+          data: prontoData,
+        }, {
+          label: "Backlog",
+          fill: true,
+          backgroundColor: colors.red.fill,
+          pointBackgroundColor: colors.red.stroke,
+          borderColor: colors.red.stroke,
+          data: backlodData,
         }]
-      },      
+      },
       options: {
         responsive: true,
         scales: {
