01f0593e15dc48589bd9f8d78546fe25c6f9a1a8
Lucas Marques <lucas.fonseca@itixti.com.br>
2023-10-05 21:39:23 -0300

    feat: correções no gráfico

--- src/app/cfd-graphic/cfd-graphic.component.ts
+++ src/app/cfd-graphic/cfd-graphic.component.ts
@@ -15,84 +15,98 @@ export class CfdGraphicComponent implements OnInit {
 
     const colors = {
       green: {
-        fill: '#a5d2a5',
-        stroke: '#329932',
+        fill: '#c2e5cb',
+        stroke: '#34a853',
       },
-      lightBlue: {
-        stroke: '#6fccdd',
-      },
-      darkBlue: {
-        fill: '#92bed2',
-        stroke: '#3282bf',
-      },
-      purple: {
-        fill: '#8fa8c8',
-        stroke: '#75539e',
+      blue: {
+        fill: '#c6dafc',
+        stroke: '#4285f4',
       },
       yellow: {
-        fill: '#fff999',
-        stroke: '#fff200'
+        fill: '#feebb3',
+        stroke: '#fbbc04'
       },
       red: {
-        fill: '#ff7a7a',
-        stroke: '#ff3232'
+        fill: '#f9c6c2',
+        stroke: '#ea4335'
       }
     };
 
     const labels = ["Sprint 1", "Sprint 2", "Sprint 3", "Sprint 4", "Sprint 5", "Sprint 6", "Sprint 7", "Sprint 8", "Sprint 9", "Sprint 10"];
-    const backlogData = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110];
-    const desenvolvimentoData = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-    const testData = [5, 15, 25, 35, 45, 55, 65, 75, 85, 95];
-    const prontoData = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+
+    const testData = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45];
+    const desenvolvimentoData = [0, 5, 10, 15, 40, 50, 60, 70, 80, 90];
+    const prontoData = [0, 5, 10, 15, 42, 25, 30, 35, 40, 45];
+    const backlogData = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
 
     const cfd = new Chart("cfd", {
       type: 'line',
       data: {
         labels: labels,
-        datasets: [{
-          label: "Teste",
-          fill: true,
-          backgroundColor: colors.darkBlue.fill,
-          pointBackgroundColor: colors.darkBlue.stroke,
-          borderColor: colors.darkBlue.stroke,
-          borderCapStyle: 'butt',
-          data: testData,
-
-        }, {
-          label: "Desenvolvimneto",
-          fill: true,
-          backgroundColor: colors.yellow.fill,
-          pointBackgroundColor: colors.yellow.stroke,
-          borderColor: colors.yellow.stroke,
-          borderCapStyle: 'butt',
-          data: desenvolvimentoData,
-        },
-        {
-          label: "Pronto",
-          fill: true,
-          backgroundColor: colors.green.fill,
-          pointBackgroundColor: colors.green.stroke,
-          borderColor: colors.green.stroke,
-          borderCapStyle: 'butt',
-          data: prontoData,
-        }, {
-          label: "Backlog",
-          fill: true,
-          backgroundColor: colors.red.fill,
-          pointBackgroundColor: colors.red.stroke,
-          borderColor: colors.red.stroke,
-          data: backlogData,
-        }]
+        datasets: [
+          {
+            label: "Pronto",
+            fill: true,
+            backgroundColor: colors.green.fill,
+            pointBackgroundColor: colors.green.stroke,
+            borderColor: colors.green.stroke,
+            data: prontoData,
+            stack: 'Stack 0'
+          },
+          {
+            label: "Teste",
+            fill: true,
+            backgroundColor: colors.blue.fill,
+            pointBackgroundColor: colors.blue.stroke,
+            borderColor: colors.blue.stroke,
+            data: testData,
+            stack: 'Stack 0'
+          },
+          {
+            label: "Desenvolvimento",
+            fill: true,
+            backgroundColor: colors.yellow.fill,
+            pointBackgroundColor: colors.yellow.stroke,
+            borderColor: colors.yellow.stroke,
+            data: desenvolvimentoData,
+            stack: 'Stack 0'
+          },
+          {
+            label: "Backlog",
+            fill: true,
+            backgroundColor: colors.red.fill,
+            pointBackgroundColor: colors.red.stroke,
+            borderColor: colors.red.stroke,
+            data: backlogData,
+            stack: 'Stack 0'
+          }]
       },
       options: {
         responsive: true,
         scales: {
-          yAxes: {
-            stacked: true
-          }
+          x: {
+            title: {
+              display: true,
+              text: 'Sprints',
+              font: {
+                weight: 'bold'
+              }
+            },
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Histórias de usuário',
+              font: {
+                weight: 'bold'
+              }
+            },
+            stacked: true // Empilhe os valores no eixo Y
+          },
         },
       },
     });
-  }
 
+  }
 }
